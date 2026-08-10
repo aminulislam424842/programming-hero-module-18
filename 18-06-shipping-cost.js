69653125a986f78c98e7f5d8d123521b34aef25f
Aminul islam <aminulislam424842@gmail.com>
2026-08-10 15:48:12 +0600

    feat: calculate shipping cost by weight

---
+++ 18-06-shipping-cost.js
@@ -0,0 +1,5 @@
+const calculateShippingCost = (weight) => 50 + (weight * 10); 
+
+console.log(calculateShippingCost(2))
+console.log(calculateShippingCost(5))
+console.log(calculateShippingCost(10))
