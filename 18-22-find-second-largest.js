bece0d6b415d1eda003d45e91054243b8969a7f5
Aminul islam <aminulislam424842@gmail.com>
2026-08-13 14:43:15 +0600

    feat: find second largest using Set and sort

--- 18-22-find-second-largest.js
+++ 18-22-find-second-largest.js
@@ -1,23 +1,41 @@
-const findSecondLargest = (arr) => {
-  const result = arr.reduce((acc, current) => {
-    if (current > acc.largest) {
-      acc.secondLargest = acc.largest;
-      acc.largest = current;
-    }
-    else if (current > acc.secondLargest && current < acc.largest
-    ) {
-      acc.secondLargest = current;
-    }
-    return acc;
-  },
-    {
-      largest: -Infinity,
-      secondLargest: -Infinity
-    })
-  return result.secondLargest
-}
+// const findSecondLargest = (arr) => {
+//   const result = arr.reduce((acc, current) => {
+//     if (current > acc.largest) {
+//       acc.secondLargest = acc.largest;
+//       acc.largest = current;
+//     }
+//     else if (current > acc.secondLargest && current < acc.largest
+//     ) {
+//       acc.secondLargest = current;
+//     }
+//     return acc;
+//   },
+//     {
+//       largest: -Infinity,
+//       secondLargest: -Infinity
+//     })
+//   return result.secondLargest
+// }
 
+
+// const numbers = [10, 5, 20, 8, 15];
+
+// console.log(findSecondLargest(numbers));
+
+
+//       use set
+
+const findSecondLargest = (arr) => {
+  const uniqueNumbers = [...new Set(arr)];
+
+  uniqueNumbers.sort((a,b) => b-a);
+
+  return uniqueNumbers.length < 2 ? null : uniqueNumbers[1];
+}
 
 const numbers = [10, 5, 20, 8, 15];
 
+const number = [5,5,5,5]
+
 console.log(findSecondLargest(numbers));
+console.log(findSecondLargest(number));
